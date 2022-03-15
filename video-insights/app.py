84835959559e0f3495d5be3faabb8b8ea052a3ba
amol.deshpande@in.ibm.com <amol.deshpande@in.ibm.com>
2022-03-15 05:50:10 +0000

    local change update

--- video-insights/app.py
+++ video-insights/app.py
@@ -14,6 +14,7 @@ app.config["VERBS"] = "static/images/verbs"
 
 @app.route('/')
 def index():
+    print("My commit")
     return render_template('index.html')
 
 port = os.getenv('VCAP_APP_PORT', '8080')
